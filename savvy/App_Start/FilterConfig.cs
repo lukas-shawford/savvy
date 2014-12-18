11b0e20219e34484e5c943a8c817a37169fc46ef
Sergey Krilov <serg.kr@gmail.com>
2014-12-17 20:18:14 -0500

    Initial commit

---
+++ savvy/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace savvy
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
