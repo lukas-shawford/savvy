e4037bbf947b22cd0194760b5e5d3fae83316fd7
Sergey Krilov <serg.kr@gmail.com>
2014-12-25 15:40:53 -0500

    Rename savvy project to savvy.UI

---
+++ savvy.UI/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using savvy.UI;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace savvy.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
