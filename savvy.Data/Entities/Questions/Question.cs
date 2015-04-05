3cc669caa727ef7c186d427f92a54df291fc3ffa
Sergey Krilov <serg.kr@gmail.com>
2015-04-05 17:01:19 -0400

    Fix unique constraint on question sequence number
    Sequence number should be unique per quiz (not globally)

--- savvy.Data/Entities/Questions/Question.cs
+++ savvy.Data/Entities/Questions/Question.cs
@@ -9,13 +9,14 @@ namespace savvy.Data.Entities.Questions
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int QuestionId { get; set; }
 
-        [Index("IX_Question_SequenceNum", 1, IsUnique = true)]
+        [Index("IX_Question_SequenceNum", 2, IsUnique = true)]
         public int SequenceNum { get; set; }
 
         public string QuestionHtml { get; set; }
 
         public string SupplementalInfoHtml { get; set; }
 
+        [Index("IX_Question_SequenceNum", 1, IsUnique = true)]
         public int QuizId { get; set; }
 
         public virtual Quiz Quiz { get; set; }
